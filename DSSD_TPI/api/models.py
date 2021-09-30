a77de24228584f1d4b4495b147b20b40c9c43f08
Tomas Magnelli <magnelli.tomas@gmail.com>
2021-09-30 19:45:18 -0300

    add model

--- DSSD_TPI/api/models.py
+++ DSSD_TPI/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Associate(models.Model):
+    name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    percentage = models.DecimalField(decimal_places=2, max_digits=3)
+
+    def __str__(self):
+        return self.name + ' ' + self.last_name
+
+
+class AnonymousSociety(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    date_created = models.DateField(auto_now_add=True)
+    associates = models.ManyToManyField(Associate, related_name='anonymous_society')
+    statute = models.FileField()
+    real_address = models.CharField(max_length=255)
+    legal_address = models.CharField(max_length=255)
+    legal_representative = models.ForeignKey(Associate, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=255)
+    country = models.CharField(max_length=128, default='Argentina')
+
+    def __str__(self):
+        return self.name
+
