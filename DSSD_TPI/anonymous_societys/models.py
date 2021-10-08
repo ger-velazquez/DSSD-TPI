769e4dcae35923922003d6743e265659dccec8b4
Tomas Magnelli <magnelli.tomas@gmail.com>
2021-10-08 19:37:34 -0300

    methods creates model

--- DSSD_TPI/anonymous_societys/models.py
+++ DSSD_TPI/anonymous_societys/models.py
@@ -7,12 +7,26 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def create(cls, name):
+        country = cls.objects.create(
+            name=name,
+        )
+        return country
+
 
 class Status(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+    @classmethod
+    def create(cls, name):
+        status = cls.objects.create(
+            name=name,
+        )
+        return status
 
 
 class AnonymousSociety(models.Model):
@@ -27,6 +41,18 @@ class AnonymousSociety(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def create(cls, name, statute, real_address, legal_address, email, legal_representative = None):
+        a_society = cls.objects.create(
+            name=name,
+            statute=statute,
+            real_address=real_address,
+            legal_address=legal_address,
+            email=email,
+            legal_representative =legal_representative
+        )
+        return a_society
+
 
 class SocietyRegistration(models.Model):
     anonymous_society = models.ForeignKey(AnonymousSociety, on_delete=models.CASCADE)
@@ -34,6 +60,17 @@ class SocietyRegistration(models.Model):
     due_date = models.DateField()
     observation = models.CharField(max_length=255)
     file_number = models.CharField(max_length=255)
+
+    @classmethod
+    def create(cls, status, due_date, observation, file_number, anonymous_society=None):
+        society_re = cls.objects.create(
+            status=status,
+            due_date=due_date,
+            observation=observation,
+            file_number=file_number,
+            anonymous_society=anonymous_society
+        )
+        return society_re
 
 
 class Associate(models.Model):
@@ -45,9 +82,28 @@ class Associate(models.Model):
     def __str__(self):
         return self.name + ' ' + self.last_name
 
+    @classmethod
+    def create(cls, name, last_name, percentage, society_registration=None):
+        associate = cls.objects.create(
+            name=name,
+            last_name=last_name,
+            percentage=percentage,
+            society_registration=society_registration
+        )
+        return associate
+
 
 
 class Export(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     state = models.CharField(max_length=255)
     anonymous_society = models.ForeignKey(AnonymousSociety, on_delete=models.CASCADE)
+
+    @classmethod
+    def create(cls, country, state, anonymous_society=None):
+        export = cls.objects.create(
+            country=country,
+            state=state,
+            anonymous_society=anonymous_society
+        )
+        return export
